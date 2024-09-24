79628bbf736d560512c01343a2087ca068032c58
YutoGCN <61838168+YutoGCN@users.noreply.github.com>
2024-09-24 13:55:06 +0900

    fix file download bug

--- client_request.py
+++ client_request.py
@@ -187,7 +187,8 @@ class SolaClient:
         
         # make directory
         if not os.path.exists(os.path.dirname(output_path)):
-            os.makedirs(os.path.dirname(output_path))
+            print(output_path)
+            os.makedirs(os.path.dirname(output_path),exist_ok=True)
             
         with open(output_path, 'wb') as f:
             f.write(response.content)
